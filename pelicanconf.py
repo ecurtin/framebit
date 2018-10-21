05e1c53d9973f6135133a08d5e2690db1635c8d7
Emily Curtin <emily@framebit.org>
2018-10-20 23:10:32 -0400

    should have clean footer and big blog titles

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import date
+
+CURRENTYEAR = date.today().year
 
 # Specify a customized theme, via path relative to the settings file
 THEME = "theme"
